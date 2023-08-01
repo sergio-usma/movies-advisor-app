a1c479f63d06bbf62131d4c730c1e5c46e8b6d9c
sergio-usma <sergioandresusma@hotmail.com>
2023-07-31 22:43:43 -0500

    Add API config file

---
+++ src/api/api_config.js
@@ -0,0 +1,7 @@
+const BASE_URL = 'https://api.themoviedb.org/3';
+const BACKDROP_URL = 'https://image.tmdb.org/t/p/original/';
+const CATEGORY = '/tv';
+const SUB_CATEGORY = '/popular';
+const API_KEY = '?api_key=4309a1a8fbc043e11ef87aa7e5c90093';
+
+export { BASE_URL, BACKDROP_URL, CATEGORY, SUB_CATEGORY, API_KEY };
