45c356641b2b1b9db1607f4be34300b105d8e66a
sergio-usma <sergioandresusma@hotmail.com>
2023-07-31 23:35:04 -0500

    Integrate StartRating into TVShow component

--- src/components/TVShow.jsx
+++ src/components/TVShow.jsx
@@ -1,4 +1,5 @@
 import style from '../styles/tvshow.module.css';
+import { FiveStarRating } from './StarRating.jsx';
 
 export function TVShowDetail({ tvShow }) {
   const rating = tvShow.vote_average / 2;
@@ -6,6 +7,7 @@ export function TVShowDetail({ tvShow }) {
     <div>
       <div className={style.title}>{tvShow.name}</div>
       <div className={style.rating_container}>
+        <FiveStarRating rating={rating} />
         <span className={style.rating}>{rating}/5</span>
       </div>
       <div className={style.overview}>{tvShow.overview}</div>
