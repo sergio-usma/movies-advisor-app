030d5b698c0539e5ddd2f09a58b2e4d15bc1e58c
sergio-usma <sergioandresusma@hotmail.com>
2023-08-01 10:41:32 -0500

    Add handler function to render recommended shows

--- src/components/PopularShowList.jsx
+++ src/components/PopularShowList.jsx
@@ -1,7 +1,7 @@
 import { PopularShowItem } from './PopularShow.jsx';
 import style from '../styles/popularshowlist.module.css';
 
-export function TVShowList({ tvShowList }) {
+export function TVShowList({ tvShowList, onClickItem }) {
   return (
     <div>
       <div className={style.title}>You'll probably like :</div>
@@ -9,7 +9,9 @@ export function TVShowList({ tvShowList }) {
         {tvShowList.map((tvShow) => {
           return (
             <span className={style.tv_show_item} key={tvShow.id}>
-              <PopularShowItem tvShow={tvShow} onClick={() => console.log('todo')} />
+              <PopularShowItem
+                onClick={onClickItem}
+                tvShow={tvShow} />
             </span>
           );
         })}
