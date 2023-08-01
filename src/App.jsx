e2a1b8ca9aabe4e715ee81bcd704b43a528bdbfa
sergio-usma <sergioandresusma@hotmail.com>
2023-08-01 11:25:24 -0500

    Render SearchBar component in the app and add handlers

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,11 @@ import { BACKDROP_URL } from './api/api_config.js';
 import { TVShowDetail } from './components/TVShow.jsx';
 import Logo from './assets/tv_logo.png';
 import { TVShowList } from './components/PopularShowList.jsx';
+import { SearchBar } from './components/SearchBar.jsx';
 
 export function App() {
-  const [currentTVShow, setcurrentTVShow] = useState();
-  const [recommendedTVShows, setrecommendedTVShows] = useState([]);
-
-  async function fetchPopulars() {
-    const popularTVShows = await TVShowAPI.fetchPopulars();
-    if (popularTVShows.length > 0) {
-      setcurrentTVShow(popularTVShows[0]);
-    }
-  }
-
-  async function fetchRecommendations(tvShowId) {
-    const recommendationListResp = await TVShowAPI.fetchRecommendations(tvShowId);
-    if (recommendationListResp.length > 0) {
-      setrecommendedTVShows(recommendationListResp.slice(0, 10));
-    }
-  }
+  const [currentTVShow, setCurrentTVShow] = useState();
+  const [recommendationList, setRecommendationList] = useState([]);
 
   useEffect(() => {
     fetchPopulars();
@@ -34,11 +21,40 @@ export function App() {
     }
   }, [currentTVShow]);
 
- function handleTVShowClick(tvShow) {
-    setcurrentTVShow(tvShow);
+  async function fetchPopulars() {
+    try {
+      const popularTVShowList = await TVShowAPI.fetchPopulars();
+      if (popularTVShowList.length > 0) {
+        setCurrentTVShow(popularTVShowList[0]);
+      }
+    } catch (error) {
+      alert('Something went wrong when fetching the popular tv shows');
+    }
+  }
+  async function fetchRecommendations(tvShowId) {
+    try {
+      const recommendationListResp = await TVShowAPI.fetchRecommendations(tvShowId);
+      if (recommendationListResp.length > 0) {
+        setRecommendationList(recommendationListResp.slice(0, 10));
+      }
+    } catch (error) {
+      alert('Something went wront fetching the recommendations');
+    }
+  }
+  function updateCurrentTVShow(tvShow) {
+    setCurrentTVShow(tvShow);
   }
 
-
+  async function fetchByTitle(title) {
+    try {
+      const searchResponse = await TVShowAPI.fetchByTitle(title);
+      if (searchResponse.length > 0) {
+        setCurrentTVShow(searchResponse[0]);
+      }
+    } catch (error) {
+      alert('Something went wrong searching a tv show');
+    }
+  }
   return (
     <div
       className={style.main_container}
@@ -52,12 +68,10 @@ export function App() {
       <div className={style.header}>
         <div className="row">
           <div className="col-4">
-            <div className={style.logo_container}>
-              <img className={style.logoImg} src={Logo} alt="Logo NinjaTV" />
-            </div>
+            <img src={Logo} alt="logo" className={style.logoImg} />
           </div>
           <div className="col-md-12 col-lg-4">
-            <input style={{ width: '100%' }} type="text" />
+            <SearchBar onSubmit={fetchByTitle} />
           </div>
         </div>
       </div>
@@ -65,7 +79,9 @@ export function App() {
         {currentTVShow && <TVShowDetail tvShow={currentTVShow} />}
       </div>
       <div className={style.recommended_shows}>
-        {currentTVShow && <TVShowList onClickItem={handleTVShowClick} tvShowList={recommendedTVShows} />}
+        {currentTVShow && (
+          <TVShowList onClickItem={updateCurrentTVShow} tvShowList={recommendationList} />
+        )}
       </div>
     </div>
   );
